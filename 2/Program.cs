db33127fbfc009c7d9e7f3212463b821134b7f68
Kolmagorov Ivan <ivandevastator27@gmail.com>
2023-01-21 12:45:04 +0300

    Второе дз

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+int numIn = InputInt ("Введите число:");
+
+if (numIn / 100 == 0)
+    {
+    Console.WriteLine ("Третьей цифры нет");
+    }
+    else
+    {
+        while (numIn > 1000)
+        {
+            numIn = numIn / 10;
+        }
+        Console.WriteLine ($"Третья цифра: {numIn % 10}"); 
+    }
