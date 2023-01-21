f99451c3e7b09ffea45b49f2cbbc5674548cf5ed
Kolmagorov Ivan <ivandevastator27@gmail.com>
2023-01-21 12:46:38 +0300

    Третье дз

---
+++ 3/Program.cs
@@ -0,0 +1,18 @@
+﻿int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+int numberDay = InputInt ("Введите день недели:");
+
+if (numberDay == 7 || numberDay == 6)
+{
+    Console.WriteLine ("Выходной.");
+}
+    else
+{
+    Console.WriteLine ("Рабочий день.");
+}
