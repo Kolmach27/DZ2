2b46b422b5e046a7ef7d14b75ae922e5da915d67
Kolmagorov Ivan <ivandevastator27@gmail.com>
2023-01-21 12:42:51 +0300

    Первое дз

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿int InputInt(string message)
+{
+    System.Console.Write(message + " > ");
+    string? inputValue = Console.ReadLine();
+    int result = Convert.ToInt32(inputValue);
+    return result;
+}
+
+bool Validate(int number)
+{
+    if (number > 99 && number < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Число не трехзначное.");
+    return false;
+}
+
+int number = InputInt("Введите трехзначное число");
+
+if (Validate(number))
+{
+    int lastRank = ((number / 10) % 10);
+    System.Console.WriteLine($"Вторая цифра числа {number} равна {lastRank} ");
+}
